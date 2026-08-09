c0a121de894e2eb2b2d014144fd182fbc519942d
Salomão Pena <spenna.live@gmail.com>
2026-08-09 00:33:46 -0300

    002

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Database\Seeders;
+
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Adicionar usuários de exemplo
+
+        $users = [
+            [
+                'email' => 'sysadmin@localhost.com',
+                'password' => bcrypt('MinhaSenha123'),
+                'id_company' => 0,
+                'role' => 'sys-admin',
+                'active' => true,
+            ],
+            [
+                'email' => 'admin@localhost.com',
+                'password' => bcrypt('MinhaSenha123'),
+                'id_company' => 1,
+                'role' => 'client-admin',
+                'active' => true,
+            ],
+            [
+                'email' => 'client@localhost.com',
+                'password' => bcrypt('MinhaSenha123'),
+                'id_company' => 2,
+                'role' => 'client-admin',
+                'active' => true,
+            ],
+        ];
+
+        DB::table('users')->insert($users);
+        echo count($users) . " usuários de exemplo adicionados com sucesso.\n";
+    }
+}
